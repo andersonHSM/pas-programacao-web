ae95ade4b9948dc1d5ed2d4f929c0d5850aa6777
Anderson Menezes <andersonhsantanappa@gmail.com>
2020-04-21 10:11:59 -0300

    frontend locations structure

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LocationsComponent } from './locations/locations.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'locations', pathMatch: 'full' },
+  { path: 'locations', component: LocationsComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ frontend/src/app/pages/locations/locations.component.ts
@@ -0,0 +1,44 @@
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-locations',
+  templateUrl: './locations.component.html',
+  styleUrls: ['./locations.component.scss'],
+})
+export class LocationsComponent implements OnInit, AfterViewInit {
+  @ViewChild('myMap', { static: false }) private gmap: ElementRef;
+
+  map: google.maps.Map;
+  marker: google.maps.Marker;
+
+  lat = -10.970027;
+  long = -37.062117;
+
+  coordinates = new google.maps.LatLng(this.lat, this.long);
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 16,
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.mapInitializer();
+  }
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+    this.marker = new google.maps.Marker({
+      position: this.coordinates,
+      map: this.map,
+    });
+  }
+}
